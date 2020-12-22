cb242002833e80b016f3dd774294d07e1f9a1004
RusuRaluca98 <62661989+RusuRaluca98@users.noreply.github.com>
2020-12-22 15:11:53 +0200

    Add files via upload

---
+++ RusuRaluca/Proiect/Samples/StackUnderflow.API.Rest/Controllers/QuestionController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using Access.Primitives.IO;
+using Microsoft.AspNetCore.Mvc;
+
+using StackUnderflow.EF.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StackUnderflow.Domain.Core.Contexts.Question;
+using LanguageExt;
+using StackUnderflow.Domain.Core.Contexts.Question;
+using StackUnderflow.Domain.Schema.Backoffice.CreateTenantOp;
+using StackUnderflow.Domain.Schema.Backoffice.InviteTenantAdminOp;
+using System.Linq;
+
+using System;
+
+
+
+namespace StackUnderflow.API.AspNetCore.Controllers
+{
+	public class QuestionController : ControllerBase
+	{
+
+        [ApiController]
+        [Route("questions")]
+        public class QuestionsController : ControllerBase
+    {
+        private readonly IInterpreterAsync _interpreter;
+        private readonly DatabaseContext _dbContext;
+
+        public QuestionsController(IInterpreterAsync interpreter, DatabaseContext dbContext)
+        {
+            _interpreter = interpreter;
+            _dbContext = dbContext;
+
+        }
+        [HttpPost("createQuestion")]
+        public async Task<IActionResult> CreateQuestion([FromBody] CreateQuestionCmd cmd)
+        {
+            var dep = new QuestionDependencies();
+
+            var questions = await _dbContext.QuestionModel.ToListAsync();
+
+            var ctx = new QuestionWriteContext(questions);
+
+            var expr = from CreateTenantResult in QuestionContext.CreateQuestion(cmd)
+                       select CreateTenantResult;
+
+            var r = await _interpreter.Interpret(expr, ctx, dep);
+
+
+            _dbContext.QuestionModel.Add(new DatabaseModel.Models.QuestionModel { Title = cmd.Title, Description = cmd.Description, Tags = cmd.Tags });
+            //var reply = await _dbContext.QuestionModel.Where(r => r.Title == "Intrebarea1").SingleOrDefaultAsync();
+            //_dbContext.QuestionModel.Update(reply);
+            await _dbContext.SaveChangesAsync();
+
+            return r.Match(
+                succ => (IActionResult)Ok(succ),
+                fail => BadRequest("Reply could not be added")
+                );
+        }
+}
