b7de721589e0c5f33d9e3c5f6900a72e3b7389db
RusuRaluca98 <62661989+RusuRaluca98@users.noreply.github.com>
2021-01-01 17:59:01 +0200

    Add files via upload

---
+++ RusuRaluca/Proiect/Samples/StackUnderflow.Schema/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackUnderflow.DatabaseModel.Models
+{
+    [Table("Questions")]
+    public class Questions
+    {
+        [Key]
+        public Guid QuestionId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Tags { get; set; }
+    }
+}
