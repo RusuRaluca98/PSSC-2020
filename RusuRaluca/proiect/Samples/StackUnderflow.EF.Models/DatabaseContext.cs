944b8d421c350a11e9e43658c3c5939f037528e0
RusuRaluca98 <62661989+RusuRaluca98@users.noreply.github.com>
2021-01-04 18:30:48 +0200

    Add files via upload

---
+++ RusuRaluca/proiect/Samples/StackUnderflow.EF.Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackUnderflow.EF.Models
+{
+	public class DatabaseContext : UserDbContext
+	{
+        public DatabaseContext()
+        { }
+        //public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+       // { }
+        //public DbSet<QuestionDomain> QuestionModel { get; set; }
+    }
+}
